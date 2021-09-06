926ddc79aebd13d2e5c976754d31c2e13c0b78b5
GerasimGerasimov <dialix@yandex.ru>
2021-09-06 17:01:34 +0700

    первый запуск

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,20 @@
 /**TODO получение IP адреса
  * hostname -I
  */
-const IP: string = '';
-console.log(`IP: ${IP}`);
+ const exec = require('child_process').exec;
+
+ async function execShellCommand(cmd): Promise<string> {
+  return new Promise((resolve, reject) => {
+   exec(cmd, (error, stdout, stderr) => {
+    if (error) {
+     console.warn(error);
+    }
+    resolve(stdout? stdout : stderr);
+   });
+  });
+ }
+
+(async()=>{
+  const IP: string = await execShellCommand('hostname -I');
+  console.log(`IP: ${IP}`);
+})()
